42d5cbd0b2c76fbf67f1ca40f27d8b063610a4a8
Christoph Rudolf <christoph.rudolf@tum.de>
2018-11-16 23:57:12 +0100

    Initial commit

---
+++ defuseapp/app/src/main/java/de/tum/hack/defuse_app/DefuseSequenceActivity.java
@@ -0,0 +1,13 @@
+package de.tum.hack.defuse_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DefuseSequenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defuse_sequence);
+    }
+}
